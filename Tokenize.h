fe5afef9fff7a07fa766238847cd80a4f62b2edf
Leon-Francis <15290552788@163.com>
2020-11-25 18:25:06 +0800

    finished word analyze

--- Tokenize.h
+++ Tokenize.h
@@ -20,23 +20,28 @@ using namespace std;
 
 typedef struct
 {
-    string name;
-    int code;
+    string name = "";
+    int code = 0;
 } Word;
 
 typedef struct
 {
     int label;    //单词序号
-    string name;  //单词本身
-    int code;     //单词种别编码
+    string name = "";  //单词本身
+    int code = 0;     //单词种别编码
     int addr = 0; //单词在符号表中的指针
+    pair<int, int> pos; //单词在源文本中的位置
 } WordToken;
 
 typedef struct
 {
     int number;      //序号
-    string name; //名字
-    string type;     //类型
+    string name = ""; //名字
+    string type = "";     //类型
+    //存储值
+    string stringValue = "";
+    int intValue = 0;
+    double doubleValue = 0;
 
 } WordSymble;
 
